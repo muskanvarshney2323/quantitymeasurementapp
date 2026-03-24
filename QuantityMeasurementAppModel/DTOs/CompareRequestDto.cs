4f8c32dc1b20307baaf8a2df7c14352716153319
muskan <varshneymuskan077@gmail.com>
2026-03-24 12:22:02 +0530

    feat(UC17): implement Quantity Measurement APIs with EF Core ORM and Swagger documentation

--- QuantityMeasurementAppModel/DTOs/CompareRequestDto.cs
+++ QuantityMeasurementAppModel/DTOs/CompareRequestDto.cs
@@ -3,9 +3,9 @@ namespace QuantityMeasurementAppModel.DTOs
     public class CompareRequestDto
     {
         public double Value1 { get; set; }
-        public string Unit1 { get; set; }
+        public string Unit1 { get; set; } = string.Empty;
         public double Value2 { get; set; }
-        public string Unit2 { get; set; }
-        public string QuantityType { get; set; }
+        public string Unit2 { get; set; } = string.Empty;
+        public string QuantityType { get; set; } = string.Empty;
     }
 }
--- QuantityMeasurementAppModel/DTOs/ConvertRequestDto.cs
+++ QuantityMeasurementAppModel/DTOs/ConvertRequestDto.cs
@@ -3,8 +3,8 @@ namespace QuantityMeasurementAppModel.DTOs
     public class ConvertRequestDto
     {
         public double Value { get; set; }
-        public string FromUnit { get; set; }
-        public string ToUnit { get; set; }
-        public string QuantityType { get; set; }
+        public string FromUnit { get; set; } = string.Empty;
+        public string ToUnit { get; set; } = string.Empty;
+        public string QuantityType { get; set; } = string.Empty;
     }
 }
